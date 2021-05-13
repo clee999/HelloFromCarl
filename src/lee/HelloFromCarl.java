ec0aa8d3d4d28b2b77f69ebb4a3a227b47bf0a51
Elaine Henson <hen20028@byui.edu>
2021-05-13 09:58:16 +0100

    Adding reply from Elaine

--- src/lee/HelloFromCarl.java
+++ src/lee/HelloFromCarl.java
@@ -4,6 +4,7 @@ public class HelloFromCarl {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Carl.");
+        System.out.println("Hello Carl, my name is Elaine.");
 	// write your code here
     }
 }
