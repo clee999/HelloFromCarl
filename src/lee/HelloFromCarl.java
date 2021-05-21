47964f86b5abbba0494407832e0747d21609d79a
clee999 <64559134+clee999@users.noreply.github.com>
2021-05-21 22:04:15 +0100

    Changing the file back.

--- src/lee/HelloFromCarl.java
+++ src/lee/HelloFromCarl.java
@@ -3,8 +3,8 @@ package lee;
 public class HelloFromCarl {
 
     public static void main(String[] args) {
-        System.out.println("Hello, my name is Carl.");
-        System.out.println("Hello Carl, my name is Elaine.");
+        System.out.println("Hello, my name is Carl.")
+
 	// write your code here
     }
 }
